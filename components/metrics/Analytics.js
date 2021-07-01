db51dcbe328653d4fba497caf0ef885d623f1b6a
Nick Crews <nicholas.b.crews@gmail.com>
2021-07-01 15:32:30 -0800

    Scrub the last (I think) of Lee Rob

--- components/metrics/Analytics.js
+++ components/metrics/Analytics.js
@@ -1,13 +1,13 @@
 import useSWR from 'swr';
 
 import fetcher from '@/lib/fetcher';
+import constants from '@/lib/constants';
 import MetricCard from '@/components/metrics/Card';
 
 export default function Analytics() {
   const { data } = useSWR('/api/views', fetcher);
 
   const pageViews = new Number(data?.total);
-  const link = 'https://leerob.io';
 
-  return <MetricCard header="All-Time Views" link={link} metric={pageViews} />;
+  return <MetricCard header="All-Time Views" link={constants.url} metric={pageViews} />;
 }
