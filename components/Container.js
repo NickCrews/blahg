3e9a3245aed5659b4825aa17f5728fcba81efe71
Nick Crews <nicholas.b.crews@gmail.com>
2021-07-01 16:22:27 -0800

    Use ${constants.url} in meta properties
    instaed of hardcoding nickcrews.me

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+import constants from '@/lib/constants';
 import Footer from '@/components/Footer';
 import NavBar from '@/components/NavBar';
 
@@ -10,7 +11,7 @@ export default function Container(props) {
   const meta = {
     title: titlePrefix ? `${titlePrefix} - Nick Crews` : "Nick Crews – Build Stuff",
     description: `Front-end developer, JavaScript enthusiast, and course creator.`,
-    image: 'https://nickcrews.me/static/images/banner.png',
+    image: `${constants.url}/static/images/banner.png`,
     type: 'website',
     ...customMeta
   };
@@ -21,8 +22,8 @@ export default function Container(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://nickcrews.me${router.asPath}`} />
-        <link rel="canonical" href={`https://nickcrews.me${router.asPath}`} />
+        <meta property="og:url" content={`${constants.url}${router.asPath}`} />
+        <link rel="canonical" href={`${constants.url}${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Nick Crews" />
         <meta property="og:description" content={meta.description} />
