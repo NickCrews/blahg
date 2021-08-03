c282d9d35990b5b594e3b67c4cf68d201ece3265
Nick Crews <nicholas.b.crews@gmail.com>
2021-08-03 13:57:18 -0600

    Add image to /about
    make it a bit more personable

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import Container from '@/components/Container';
 import Timeline from '@/components/Timeline';
@@ -7,18 +8,23 @@ export default function About() {
   return (
     <Container titlePrefix="About">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-6 text-black dark:text-white">
           About Me
         </h1>
-        <div className="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
-          <p>
-            I'm Nick. I like to build stuff, especially when it's a pairing that
-            leverages computer science to directly solve problems in the
-            physical world. I have experience as a software engineer, physicist,
-            teacher, carpenter, and in an ideal project I get to exercise all
-            these parts of my brain.
-          </p>
-        </div>
+        <Image
+          alt={`Me, looking happy in the desert.`}
+          src={`/static/me.jpg`}
+          width={4912}
+          height={3264}
+          className="shadow-lg rounded-xl"
+        />
+        <p className="mt-4 text-gray-600 dark:text-gray-400">
+          I'm Nick. I like to build stuff, especially when it's a pairing that
+          leverages computer science to directly solve problems in the physical
+          world. I have experience as a software engineer, physicist, teacher,
+          carpenter, and in an ideal project I get to exercise all these parts
+          of my brain.
+        </p>
         <Timeline />
       </div>
     </Container>
