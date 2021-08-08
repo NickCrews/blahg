dce816e34768e0000f80d817031cc58cd7d5a900
Nick Crews <nicholas.b.crews@gmail.com>
2021-08-07 20:16:10 -0600

    Make ProjectCard always have vertical layout
    This will be useful when I'm about to add the picture above it

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -12,11 +12,11 @@ export default function ProjectCard({ title, summary, slug }) {
           height={image.height}
           width={mage.width}
         /> */}
-        <div className="flex flex-col md:flex-row justify-between">
-          <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
+        <div className="flex flex-col justify-between">
+          <h4 className="text-lg font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
             {title}
           </h4>
-          <p className="text-gray-500 text-left md:text-right w-32 mb-4 md:mb-0">
+          <p className="text-gray-500 text-left w-32 mb-4">
             <ViewsDisplay slug={slug} />
           </p>
         </div>
