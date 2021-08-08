b2b94fe67b1863277cbc393e6618c2176d2ee9b7
Nick Crews <nicholas.b.crews@gmail.com>
2021-08-07 19:43:02 -0600

    Update 404 message
    Make it say something different from Rob

--- pages/404.js
+++ pages/404.js
@@ -7,13 +7,11 @@ export default function NotFound() {
     <Container title="Page Not Found">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          451 – Unavailable For Legal Reasons
+          404 – Oopsies!
         </h1>
         <p className="text-gray-600 dark:text-gray-400 mb-8">
-          Why show a generic 404 when I can make it sound mysterious? It seems
-          you've found something that used to exist, or you spelled something
-          wrong. I'm guessing you spelled something wrong. Can you double check
-          that URL?
+          Not all who wander are lost, but it sure looks like you are. Perhaps
+          double check that URL?
         </p>
         <Link href="/">
           <a className="p-1 sm:p-4 w-64 font-bold mx-auto bg-gray-100 dark:bg-gray-900 text-center rounded-md text-black dark:text-white">
