5926e60eac41b4091def08ba2f40f1639c76220e
Nick Crews <nicholas.b.crews@gmail.com>
2021-08-07 21:02:07 -0600

    Add images to project cards + display in a grid
    I think the images are really a lot more enticing than
    the straight text that there was before.

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,26 +1,22 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import ViewsDisplay from './ViewsDisplay';
 
-export default function ProjectCard({ title, summary, slug }) {
+export default function ProjectCard({ title, summary, slug, image }) {
   return (
     <Link href={`/projects/${slug}`}>
-      <a className="w-full mb-8">
-        {/* TODO this, but I style it better first */}
-        {/* <Image
-          src={image.src}
-          height={image.height}
-          width={mage.width}
-        /> */}
-        <div className="flex flex-col justify-between">
+      <a className="mb-8 border rounded-b-xl">
+        <Image src={image.src} height={image.height} width={image.width} />
+        <div className="m-2">
           <h4 className="text-lg font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
             {title}
           </h4>
           <p className="text-gray-500 text-left w-32 mb-4">
             <ViewsDisplay slug={slug} />
           </p>
+          <p className="text-gray-600 dark:text-gray-400">{summary}</p>
         </div>
-        <p className="text-gray-600 dark:text-gray-400">{summary}</p>
       </a>
     </Link>
   );
@@ -33,6 +29,7 @@ export function fromPost(post) {
       title={post.meta.title}
       summary={post.meta.summary}
       slug={post.slug}
+      image={post.meta.image}
     />
   );
 }
