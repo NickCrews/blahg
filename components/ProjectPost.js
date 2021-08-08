970d9ad4a3936af898bb79a8b8063376b4cf8db8
Nick Crews <nicholas.b.crews@gmail.com>
2021-08-07 19:34:18 -0600

    Factor out ViewsDisplay component
    it still isn't quite unified with the page views used on the
    header for individual posts.

--- components/ProjectPost.js
+++ components/ProjectPost.js
@@ -1,12 +1,8 @@
 import Link from 'next/link';
-import useSWR from 'swr';
 
-import fetcher from '@/lib/fetcher';
+import ViewsDisplay from './ViewsDisplay';
 
 const ProjectPost = ({ title, summary, slug }) => {
-  const { data } = useSWR(`/api/views/${slug}`, fetcher);
-  const views = data?.total;
-
   return (
     <Link href={`/projects/${slug}`}>
       <a className="w-full mb-8">
@@ -21,7 +17,7 @@ const ProjectPost = ({ title, summary, slug }) => {
             {title}
           </h4>
           <p className="text-gray-500 text-left md:text-right w-32 mb-4 md:mb-0">
-            {`${views ? new Number(views).toLocaleString() : '–––'} views`}
+            <ViewsDisplay slug={slug} />
           </p>
         </div>
         <p className="text-gray-600 dark:text-gray-400">{summary}</p>
